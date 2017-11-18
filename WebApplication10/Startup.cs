b4b571dedfbfae519c48d8aee494295c7be54c16
Henrik Fischer Nielsen <hdfn@nnit.com>
2017-11-18 11:44:43 +0100

    First commit test

--- WebApplication10/Startup.cs
+++ WebApplication10/Startup.cs
@@ -23,6 +23,7 @@ namespace WebApplication10
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
